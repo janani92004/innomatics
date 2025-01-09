43662f3c9f03a686fc8208f426d2a8428ded6f65
Janani Saravana <158731761+janani92004@users.noreply.github.com>
2025-01-09 15:21:24 +0530

    Update innomatics2.py

--- innomatics2.py
+++ innomatics2.py
@@ -1,16 +1,16 @@
-
+#THIS CODE IS TO MERGE MY CLEANED HSR FILE WITH MY MAIN MERGED FILE
 import pandas as pd
 
-# Load the two CSV files
-df1 = pd.read_csv("C:\\Users\\Windows\\Desktop\\hsr.csv")  # Replace with your actual file paths
+
+df1 = pd.read_csv("C:\\Users\\Windows\\Desktop\\hsr.csv")
 df2 = pd.read_csv("C:\\Users\\Windows\\Desktop\\merged_file.csv")
 
-# Merge the two DataFrames on a common column (e.g., 'property_id')
-merged_df = pd.merge(df1, df2, on='property_id', how='inner')  # 'inner' can be changed to 'left', 'right', or 'outer'
 
-# Save the merged DataFrame to a new CSV file
+merged_df = pd.merge(df1, df2, on='property_id', how='inner')  
+
+
 merged_df.to_csv('NEW_merged_file.csv', index=False)
 
-# Print the merged DataFrame
+
 print(merged_df)
 print("DONE")
